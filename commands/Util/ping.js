223c993488604e84c1fddf1153002f6da3acac85
anmoti <77442643+anmoti@users.noreply.github.com>
2023-02-23 20:51:11 +0900

    Updated command ping
    14.7.2で変更必要

--- commands/Util/ping.js
+++ commands/Util/ping.js
@@ -1,3 +1,4 @@
+const { DiscordSnowflake } = require('@sapphire/snowflake');//14.7.2 で削除
 module.exports = {
     builder(SlashCommandBuilder) {
         SlashCommandBuilder
@@ -5,10 +6,20 @@ module.exports = {
         .setDescription("このbotのpingを測定します。")
     },
     cooldown: 0, //ms
-    message(message, args, logger) {
-        message.channel.send("pong");
+    async message(message, args, logger) {
+        const finaly = await ping(message);
+        finaly[0].edit(finaly[1]);
     },
-    interaction(interaction, logger) {
-        interaction.reply("pong");
+    async interaction(interaction, logger) {
+        const finaly = await ping(interaction);
+        interaction.editReply(finaly[1]);
     }
 };
+
+async function ping(obj) {
+    const base = ":ping_pong:Pong!\r"
+    const reply = await obj.reply(base + "取得中...");
+    const final = base + `Websocket: ${obj.client.ws.ping}ms\rAPI Endpoint: ${Date.now() - DiscordSnowflake.timestampFrom(reply.id)}ms`; //14.7.2で DiscordSnow.... を reply.createdTimestamp に変更
+    console.log(reply)
+    return [reply, final];
+}
